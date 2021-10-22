8180da0d4970b649cdab5da13bd3584011394d7d
FcoJavierGlez <FJGS_86@hotmail.com>
2021-10-22 10:19:33 +0200

    Primer commit

---
+++ src/main/java/com/nttdata/bootcamp/microserviciocloud1/MicroservicioCloud1Application.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.microserviciocloud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCloud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCloud1Application.class, args);
+	}
+
+}
